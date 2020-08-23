cf08573b364d2fbd63361817169185403bb8f057
jesus <sirijesus754@gmail.com>
2020-08-23 19:33:46 -0300

    Logica para manejo de errores

--- src/main/java/com/cashonline/app/config/ExceptionConfig.java
+++ src/main/java/com/cashonline/app/config/ExceptionConfig.java
@@ -1,7 +1,7 @@
 package com.cashonline.app.config;
 
-
-
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,29 +11,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cashonline.app.config.exception.BadRequestException;
+import com.cashonline.app.config.exception.InternalServerErrorException;
 import com.cashonline.app.config.exception.NotFoundException;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ExceptionConfig {
 	
-	
 	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<?> notFoundException(Exception e) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.contentType(MediaType.APPLICATION_JSON)
-				.body(e.getMessage());
+				.body(createResponse(e));
 	}
 	
 	
-	@ExceptionHandler(BadRequestException.class)
+	@ExceptionHandler({BadRequestException.class})
 	public ResponseEntity<?> badRequestException(Exception e) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(createResponse(e));
 	}
 	
-
-		    		
-		    	
+	@ExceptionHandler({InternalServerErrorException.class})
+	public ResponseEntity<?> InternalServerErrorException(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(createResponse(e));
+	}
+	
+	
+	private Map<String,Object> createResponse(Exception e) {
+		Map<String,Object> response = new HashMap<>();
+		response.put("message", "Ocurrio un error");
+		response.put("error", e.getMessage()); 
+		return response;
+	}
+	
 		
-	
-	
 }
