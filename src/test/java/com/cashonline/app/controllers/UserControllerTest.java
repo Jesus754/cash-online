97b4be8648b0b9d0362aab7150122fe17fa74936
jesus <sirijesus754@gmail.com>
2020-08-25 16:03:51 -0300

    finalizo test para UserController

--- src/test/java/com/cashonline/app/controllers/UserControllerTest.java
+++ src/test/java/com/cashonline/app/controllers/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.cashonline.app.controllers;
 
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,7 +10,6 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -25,55 +23,78 @@ import org.springframework.web.context.WebApplicationContext;
 import com.cashonline.app.CashOnlineApplication;
 import com.cashonline.app.models.entity.User;
 import com.cashonline.app.models.repository.IUserRepository;
-import com.cashonline.app.service.IUserService;
-import com.cashonline.app.service.UserServiceImpl;
+import com.google.gson.Gson;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.MOCK, classes={ CashOnlineApplication.class})
- 
+@SpringBootTest(webEnvironment=WebEnvironment.MOCK, classes={ CashOnlineApplication.class })
 public class UserControllerTest {
-	
 
 	private MockMvc mockMvc;
-	
-	 @Autowired
-	 private WebApplicationContext webApplicationContext;
-	 
-	 @MockBean
-	 private IUserRepository UserRepositoryMock;
 
-	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@MockBean
+	private IUserRepository UserRepositoryMock;
+
 	@Before
 	public void setUp() throws Exception {
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
-	
+
 	@Test
-	public void shouldGetUserWhenValidRequest() throws Exception {
-		User user = new User("SiriJesus754@gmail.com","Jesus", "Siri");
-		
+	public void shouldGetUserWhenValidRequest_Get_User_Method() throws Exception {
+		User user = new User("SiriJesus754@gmail.com", "Jesus", "Siri");
+
 		when(UserRepositoryMock.findById(1L)).thenReturn(Optional.of(user));
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", 1)
-				.accept(MediaType.APPLICATION_JSON))
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", 1).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.email").value(user.getEmail()))
 				.andExpect(jsonPath("$.firstName").value(user.getFirstName()))
 				.andExpect(jsonPath("$.lastName").value(user.getLastName()));
 	}
-	
+
 	@Test
-	public void shouldReturn404WhenUserNotFound() throws Exception {
-		
+	public void shouldReturn404WhenUserNotFound_Get_User_Method() throws Exception {
+
 		when(UserRepositoryMock.findById(2L)).thenReturn(Optional.empty());
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", 2)
-				.accept(MediaType.APPLICATION_JSON))
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", 2).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound());
 	}
 	
+	@Test 
+	public void shouldCreateUserWhenValidRequest_Create_User_Method() throws Exception {
+		Gson gson = new Gson();
+		
+		User user = new User("SiriJesus754@gmail.com", "Jesus", "Siri");
+		
+		when(UserRepositoryMock.save(user)).thenReturn(user);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/users").contentType(MediaType.APPLICATION_JSON)
+				.content(gson.toJson(user))
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());	
+	}
 	
+	@Test 
+	public void ShouldDeleteUserWhenValidRequest_Delete_User_Method() throws Exception {
+		User user = new User("SiriJesus754@gmail.com", "Jesus", "Siri");
+		
+		when(UserRepositoryMock.findById(3L)).thenReturn(Optional.of(user));
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/users/{id}",3).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());	
+	}
 	
-	
+	@Test
+	public void shouldReturn404WhenUserNotFound_Delete_Method() throws Exception {
+		
+		when(UserRepositoryMock.findById(3L)).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/users/{id}",3).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());	
+	}
 	
 }
